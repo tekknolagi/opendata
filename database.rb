b483a05a43b462087a83eb94cfdf492475c5207c
Max Bernstein <tekknolagi@gmail.com>
2013-05-23 19:19:59 -0700

    wrong url

--- database.rb
+++ database.rb
@@ -2,7 +2,7 @@ require 'data_mapper'
 require 'dm-postgres-adapter'
 
 if ENV['MODE'] == 'travis'
-  DataMapper.setup :default, 'postgresql://postgres@127.0.0.1/opendata'
+  DataMapper.setup :default, 'postgres://postgres@127.0.0.1/opendata'
 else
   DataMapper.setup :default, ENV['HEROKU_POSTGRESQL_COPPER_URL']
 end
